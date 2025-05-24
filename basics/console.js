5b5c1992c48dc35f9b96c7b6e8fc9c843536316e
glennnoronha <glenn.noronha@yahoo.com>
2025-05-24 15:35:48 -0500

    intitial setup and start of basics

---
+++ basics/console.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+document.getElementById('myH1').textContent = 'Console';
+document.getElementById('myP').textContent = 'The console is a place where you can see the output of your code';
+
+// window.alert('This is a window alert')
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+let x = 10;
+console.log(x);
+
+let age = 25;
+let firstName = 'Glenn';
+let graduated = true;
+
+console.log(age, typeof age)
+console.log(firstName, typeof firstName)
+console.log(graduated, typeof graduated)
+
+let fullName = "Glenn Noronha";
+let country = "Canada";
+let birthYear = 2002;
+let student = false;
